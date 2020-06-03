7984b2c39f85dc7b720712df600287ef00a4d400
Avneesh Kumar <avneesh.kumar@gmail.com>
2020-06-02 19:20:42 -0700

    converted currency to decimal

--- db/migrate/20141106034610_add_currency_to_payola_subscriptions.rb
+++ db/migrate/20141106034610_add_currency_to_payola_subscriptions.rb
@@ -1,6 +1,6 @@
 class AddCurrencyToPayolaSubscriptions < ActiveRecord::Migration[4.2]
   def change
     add_column :payola_subscriptions, :currency, :string
-    add_column :payola_subscriptions, :amount, :integer
+    add_column :payola_subscriptions, :amount, :decimal, :precision => 8, :scale => 2
   end
 end
